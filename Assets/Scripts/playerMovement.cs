790e0d1a6b3058e37fb9ce6fb7ec0cad277a3246
AshfordGraye <117757557+AshfordGraye@users.noreply.github.com>
2024-05-08 07:37:23 +0100

    player and camera movement improvement
    improved player and camera movement. player will now move relative to the camera position. Gravity has also been fixed using a method to increase gravitational force at the game start.

--- Assets/Scripts/playerMovement.cs
+++ Assets/Scripts/playerMovement.cs
@@ -1,20 +1,19 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Numerics;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class Player : MonoBehaviour
 {
 
     [SerializeField] float speedMove;
-    [SerializeField] float speedTurn;
     [SerializeField] float speedStrafe;
 
     // Start is called before the first frame update
     void Start()
     {
-        speedMove = 10;
-        speedTurn = 100;
-        speedStrafe = 10;
+        PhysicsValues();
     }
 
     // Update is called once per frame
@@ -23,15 +22,24 @@ public class Player : MonoBehaviour
         MovePlayer();
     }
 
-    void MovePlayer()
+    void PhysicsValues()
     {
-        float playerForwardBack = Input.GetAxis("Vertical") * Time.deltaTime * speedMove;
-        transform.Translate(0, 0, playerForwardBack);
-        float playerTurn = Input.GetAxis("Horizontal") * Time.deltaTime * speedTurn;
-        // transform.Rotate(0, playerTurn, 0);
-        float playerStrafe = Input.GetAxis("Horizontal") * Time.deltaTime * speedStrafe;
-        transform.Translate(playerStrafe, 0, 0);
+        speedMove = 2000;
+        speedStrafe = 2000;
+        Physics.gravity = new UnityEngine.Vector3 (0,-500,0);
     }
 
+    void MovePlayer()
+    {
+        // float playerForwardBack = Input.GetAxis("Vertical") * Time.deltaTime * speedMove;
+        // transform.Translate(0, 0, playerForwardBack);
+        // float playerTurn = Input.GetAxis("Horizontal") * Time.deltaTime * speedTurn;
+        // transform.Rotate(0, playerTurn, 0);
+        // float playerStrafe = Input.GetAxis("Horizontal") * Time.deltaTime * speedStrafe;
+        // transform.Translate(playerStrafe, 0, 0);
 
+        var Player = GetComponent<Rigidbody>();
+        UnityEngine.Vector3 movement = UnityEngine.Quaternion.Euler(0, Camera.main.transform.eulerAngles.y, 0) * new UnityEngine.Vector3(Input.GetAxis("Horizontal") * speedStrafe * Time.deltaTime, 0, Input.GetAxis("Vertical") * speedMove * Time.deltaTime);
+        Player.velocity = movement;
+    }
 }
