707055024e4138e3f89524c8ae6cad1344878738
AshfordGraye <117757557+AshfordGraye@users.noreply.github.com>
2024-05-15 12:48:36 +0100

    Fixed issues with lost data
    Lost data due to not saving within Unity. This commit fixes those issues
    
    Added: Bouncy panel to double player bounce height.

--- Assets/Scripts/playerMovement.cs
+++ Assets/Scripts/playerMovement.cs
@@ -10,21 +10,37 @@ using UnityEngine;
 public class Player : MonoBehaviour
 {
 
-    [SerializeField] float forceMove;
-    [SerializeField] float forceStrafe;
-
-    [SerializeField] int forceGrav;
-    [SerializeField] int forceJump;
-
-    [SerializeField] UnityEngine.Vector3 movement;
 
 
     public static Rigidbody PlayerBody;
     public static bool playerJumpEnabled;
-    public static float baseforceDrag = 0.5f;
-    public static float baseforceAngularDrag = 0.5f;
-
-
+    // BASE VALUES FOR PLAYER PHYSICS
+    public static float baseforceMove = 100;
+    public static int baseforceJump = 100;
+    public static float baseforceDrag = 1f;
+    public static float baseforceAngularDrag = 1f;
+    //  PLAYER PHYSICS VALUES TO BE USED DURING PLAY. 
+    public static float forceMove;
+    public static int forceJump;
+    void PhysicsValues()
+    {
+        // AT GAME START, SET VALUES TO DEFAULTS DETERMINED BY DIFFICULTY 
+        forceMove = baseforceMove;
+        forceJump = baseforceJump;
+    }
+    void MovePlayer()
+    {
+        UnityEngine.Vector3 movement = UnityEngine.Quaternion.Euler(0, Camera.main.transform.eulerAngles.y, 0) * new UnityEngine.Vector3(Input.GetAxis("Horizontal") * forceMove * Time.deltaTime, 0, Input.GetAxis("Vertical") * forceMove * Time.deltaTime);
+        PlayerBody.AddForce(movement, ForceMode.Force);
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            Debug.Log("Space Key Pressed");
+            if (playerJumpEnabled)
+            {
+                PlayerBody.AddForce(UnityEngine.Vector3.up * forceJump, ForceMode.Force);
+            }
+        }
+    }
     // Start is called before the first frame update
     void Start()
     {
@@ -36,27 +52,5 @@ public class Player : MonoBehaviour
     void Update()
     {
         MovePlayer();
-    }
-
-    void PhysicsValues()
-    {
-        forceMove = 100;
-        forceStrafe = 100;
-        forceJump = 100;
-    }
-
-    void MovePlayer()
-    {
-        UnityEngine.Vector3 movement = UnityEngine.Quaternion.Euler(0, Camera.main.transform.eulerAngles.y, 0) * new UnityEngine.Vector3(Input.GetAxis("Horizontal") * forceStrafe * Time.deltaTime, 0, Input.GetAxis("Vertical") * forceMove * Time.deltaTime);
-        PlayerBody.AddForce(movement, ForceMode.Force);
-
-        if (Input.GetKeyDown(KeyCode.Space))
-        {
-            Debug.Log("Space Key Pressed");
-            if (playerJumpEnabled)
-            {
-                PlayerBody.AddForce(UnityEngine.Vector3.up * forceJump, ForceMode.Force);
-            }
-        }
     }
 }
